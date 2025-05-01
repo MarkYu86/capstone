0596d28fe21e0478fbacdaa9eb764c74ccf15959
Mark Zihao Yu <yuzihao.1997@gmail.com>
2025-05-01 12:28:23 +1200

    final commit before

--- frontend/src/pages/CalendarPage.jsx
+++ frontend/src/pages/CalendarPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../index.css";
 import "../styles/Calendar.css"
+import "../index.css"
 
 function CalendarPage() {
   const [tasks, setTasks] = useState([]);
